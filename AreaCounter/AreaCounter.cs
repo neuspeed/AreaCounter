9badda6f98203fbd867a44fd0ae7d9ece8ef24b1
Mikhail <neuspeed@inbox.ru>
2019-06-18 17:11:41 +0300

    First Commit

---
+++ AreaCounter/AreaCounter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCounter
+{
+    public static class AreaCounter
+    {
+
+    }   
+    
+}
